c9951a327578445c9aedd0b3ceefb719bea061b6
MolikoDeveloper <58595683+MolikoDeveloper@users.noreply.github.com>
2023-09-15 16:15:48 -0300

    Add files via upload

---
+++ Console_Plugin.cs
@@ -0,0 +1,11 @@
+using Godot;
+
+[Tool]
+public partial class Console_Plugin : EditorPlugin
+{
+    public override void _EnterTree()
+    {
+        base._EnterTree();
+        AddAutoloadSingleton("CommandConsole", "res://addons/csharpConsole/CommandConsole.cs");
+    }
+}
